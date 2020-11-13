e9b68f7996023b06988d3a71f833639ef3f6261b
gnik-snrub <60440393+gnik-snrub@users.noreply.github.com>
2020-11-13 23:20:46 +1100

    Added HTML/CSS calculator scaffolding. Added power + factorial functions.

--- index.js
+++ index.js
@@ -14,6 +14,22 @@ function divide(a, b) {
     return a / b;
 }
 
+function power(val, mult) {
+	let pow = val;
+	for (i = 1; i < mult; i++) {
+		pow *= val;
+	}
+	return pow;
+}
+
+function factorial(num) {
+	if (num == 0 || num == 1) { return 1; }
+	for (i = num - 1; i >= 1; i--) {
+		num *= i;
+	}
+	return num;
+}
+
 function operate(numOne, numTwo, operator) {
     switch (operator) {
         case "add":
@@ -24,5 +40,9 @@ function operate(numOne, numTwo, operator) {
             return multiply(numOne, numTwo);
         case "div":
             return divide(numOne, numTwo);
+        case "pow":
+            return power(numOne, numTwo);
+        default:
+            return factorial(numOne);
     }
 }
